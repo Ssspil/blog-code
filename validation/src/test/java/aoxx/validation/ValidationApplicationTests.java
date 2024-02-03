a25f81bddc8f001cab37946786b89acc5b6fbcd4
Ssspil <rius0918@gmail.com>
2024-02-03 20:36:51 +0900

    생성

---
+++ validation/src/test/java/aoxx/validation/ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package aoxx.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
