6e8399e84a1a4f545ff61fa99b83d4b03055aa8e
Ssspil <rius0918@gmail.com>
2024-02-12 00:26:55 +0900

    파일명 변경 Handler -> Advice

--- validation/src/main/java/aoxx/validation/domain/exception/ValidControllerAdvice.java
+++ validation/src/main/java/aoxx/validation/domain/exception/ValidControllerAdvice.java
@@ -1,6 +1,7 @@
 package aoxx.validation.domain.exception;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -13,8 +14,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
-public class ValidErrorHandler {
+public class ValidControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -22,11 +24,17 @@ public class ValidErrorHandler {
         BindingResult bindingResult = e.getBindingResult();
         List<ObjectError> globalErrors = bindingResult.getGlobalErrors();
 
+
+        for (ObjectError globalError : globalErrors) {
+            System.out.println("globalError :" +  globalError);
+            System.out.println("globalError : " + globalError.getDefaultMessage());
+        }
+
         Map<String, Object> data = new HashMap<>();
         data.put("connStatus" , "fail");
         data.put("Errors" , globalErrors);
 
-        return new ErrorResult(400, e.getMessage(), data);
+        return new ErrorResult(400, e.toString(), data);
     }
 
 }
