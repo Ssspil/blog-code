3c60fb9a313af16adc6699d619131f8916b807dd
Ssspil <rius0918@gmail.com>
2024-02-15 00:03:14 +0900

    유효성 검사 예외 처리 객체와 로직 생성 끝.

--- validation/src/main/java/aoxx/validation/domain/exception/ErrorResult.java
+++ validation/src/main/java/aoxx/validation/domain/exception/ErrorResult.java
@@ -1,5 +1,6 @@
 package aoxx.validation.domain.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,16 +8,11 @@ import java.util.Map;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class ErrorResult {
 
     private Integer code;
     private String message;
     private Map<String, Object> data;
 
-
-    public ErrorResult(Integer code, String message, Map<String, Object> data) {
-        this.code = code;
-        this.message = message;
-        this.data = data;
-    }
 }
--- validation/src/main/java/aoxx/validation/domain/exception/ValidControllerAdvice.java
+++ validation/src/main/java/aoxx/validation/domain/exception/ValidControllerAdvice.java
@@ -4,37 +4,41 @@ package aoxx.validation.domain.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(annotations = RestController.class)
 public class ValidControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResult MethodArgumentNotValidExHandler(MethodArgumentNotValidException e){
-        BindingResult bindingResult = e.getBindingResult();
-        List<ObjectError> globalErrors = bindingResult.getGlobalErrors();
-
-
-        for (ObjectError globalError : globalErrors) {
-            System.out.println("globalError :" +  globalError);
-            System.out.println("globalError : " + globalError.getDefaultMessage());
-        }
-
+    public ErrorResult MethodArgumentNotValidExHandler(MethodArgumentNotValidException e) {
         Map<String, Object> data = new HashMap<>();
-        data.put("connStatus" , "fail");
-        data.put("Errors" , globalErrors);
 
-        return new ErrorResult(400, e.toString(), data);
+        List<Map<String, Object>> list = new ArrayList<>();
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            Map<String, Object> field = new HashMap<>();
+            field.put("field", fieldError.getField());
+            field.put("msg", fieldError.getDefaultMessage());
+            field.put("rejectedValue", fieldError.getRejectedValue());
+
+            list.add(field);
+        }
+        data.put("connStatus", "fail");
+        data.put("errorList", list);
+
+        return new ErrorResult(HttpStatus.BAD_REQUEST.value(), "실패", data);
     }
-
 }
