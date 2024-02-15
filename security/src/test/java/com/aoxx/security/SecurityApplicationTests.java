4e56beeaede22437b377edfcfa1030bcd3524bc0
Ssspil <rius0918@gmail.com>
2024-02-15 23:02:14 +0900

    스프링 시큐리티 프로젝트 생성

---
+++ security/src/test/java/com/aoxx/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aoxx.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
