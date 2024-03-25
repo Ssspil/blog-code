1ca49b56531e5d580a5d8a4ce22db1b12d42ae74
Ssspil <rius0918@gmail.com>
2024-03-25 23:38:50 +0900

    프로젝트 생성할 때 따라 올라간것 코드 넣고 수정

--- security/src/main/java/com/aoxx/security/domain/Member.java
+++ security/src/main/java/com/aoxx/security/domain/Member.java
@@ -1,2 +1,21 @@
-package com.aoxx.security.domain;public class Member {
+package com.aoxx.security.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
 }
