2d497f1ac005fa61c2b25126909eefabe2d515ac
Ssspil <rius0918@gmail.com>
2024-05-06 22:09:34 +0900

    항공예매 시스템 도메인 예제

---
+++ airline-reserve/src/test/java/com/airline/reserve/ReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline.reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
