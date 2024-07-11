edf5125bdfe6cc29a7b764ee7376257a97a1d995
Ssspil <rius0918@gmail.com>
2024-07-12 00:54:44 +0900

    CORS 설정

---
+++ security/src/main/java/com/aoxx/security/config/WebConfig.java
@@ -0,0 +1,26 @@
+package com.aoxx.security.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${spring.security.cors.allowed-origins}")
+    private String  ALLOW_CROSS_ORIGIN_DOMAIN;
+
+    @Value("${spring.security.cors.allowed-methods}")
+    private String[] ALLOW_METHODS;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(ALLOW_CROSS_ORIGIN_DOMAIN)
+                .allowedMethods(ALLOW_METHODS)
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
