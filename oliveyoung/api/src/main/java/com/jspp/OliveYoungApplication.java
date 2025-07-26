b5f187f61b4bfc35599fec608fcecfdd062e953b
Ssspil <rius0918@gmail.com>
2025-07-27 00:52:06 +0900

    멀티 모듈 세팅 및 CMS 개별 기동 때 포트 변경

---
+++ oliveyoung/api/src/main/java/com/jspp/OliveYoungApplication.java
@@ -0,0 +1,11 @@
+package com.jspp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.jspp")
+public class OliveYoungApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OliveYoungApplication.class);
+    }
+}
