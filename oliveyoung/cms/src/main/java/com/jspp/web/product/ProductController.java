f48211cc96a08d389cf1cabf671285feaf4ee7cb
Ssspil <rius0918@gmail.com>
2025-08-25 22:46:43 +0900

    상품 목록 UI 및 레이아웃

---
+++ oliveyoung/cms/src/main/java/com/jspp/web/product/ProductController.java
@@ -0,0 +1,17 @@
+package com.jspp.web.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+
+    // 상품 목록
+    @GetMapping
+    public String products() {
+        return "/page/product/list";
+    }
+
+}
